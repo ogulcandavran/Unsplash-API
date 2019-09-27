a09094f7ea523f40cfd1701ef67b151601894400
ogulcandavran <ogulcandavran@yahoo.com>
2019-09-27 15:19:09 +0300

    InputAnimation

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,11 +1,84 @@
 import React from "react";
+import anime from "animejs";
+import { delay } from "q";
 
 class SearchBar extends React.Component {
   state = { term: "" };
+  myRef = React.createRef();
 
   onFormSubmit = event => {
     event.preventDefault();
     this.props.onFormSubmit(this.state.term);
+  };
+
+  focusAnime = () => {
+    const tl = anime.timeline({
+      easing: "easeInQuad",
+      duration: 200
+    });
+    tl.add({
+      targets: this.myRef.current,
+      borderLeftColor: "#4267b2"
+    })
+      .add({
+        targets: this.myRef.current,
+        borderTopColor: "#4267b2"
+      })
+      .add({
+        targets: this.myRef.current,
+        borderRightColor: "#4267b2"
+      })
+      .add({
+        targets: this.myRef.current,
+        borderBottomColor: "#4267b2"
+      })
+      .add({
+        targets: this.myRef.current,
+        borderLeftColor: "#ffffff"
+      })
+      .add({
+        targets: this.myRef.current,
+        borderTopColor: "#ffffff"
+      })
+      .add({
+        targets: this.myRef.current,
+        borderRightColor: "#ffffff"
+      })
+      .add({
+        targets: this.myRef.current,
+        delay: 200,
+        duration: 300,
+        borderBottomWidth: 2
+      })
+      .add({
+        targets: this.myRef.current,
+        duration: 1000,
+        borderBottomColor: "#000000"
+      });
+  };
+
+  blurAnime = () => {
+    const tl = anime.timeline({
+      easing: "easeInQuad",
+      duration: 200
+    });
+    tl.add({
+      targets: this.myRef.current,
+      borderRightColor: "#000000"
+    })
+      .add({
+        targets: this.myRef.current,
+        borderTopColor: "#000000"
+      })
+      .add({
+        targets: this.myRef.current,
+        borderLeftColor: "#000000"
+      })
+      .add({
+        targets: this.myRef.current,
+        delay: 100,
+        borderBottomWidth: 1
+      });
   };
 
   render() {
@@ -13,6 +86,9 @@ class SearchBar extends React.Component {
       <div className="searchBar">
         <form onSubmit={this.onFormSubmit}>
           <input
+            onFocus={this.focusAnime}
+            onBlur={this.blurAnime}
+            ref={this.myRef}
             placeholder="Search for images..."
             onChange={event =>
               this.setState({ term: event.target.value.toUpperCase() })
